46849f0442947432fe9d1d9782f536729fdb2ac8
Maksim Chalyh <nazgulius@yandex.ru>
2025-01-22 23:47:06 +0300

    add text in js

--- src/js/app.js
+++ src/js/app.js
@@ -1,19 +1,34 @@
 import Widget from "./Widget.js";
 
-document.addEventListener('DOMContentLoaded', () => { 
 
-  const button = document.querySelector('.button');
-  const form = document.querySelector('.form');
+const form = document.querySelector('.form');
+document.addEventListener('DOMContentLoaded', () => { 
   
 
-  button.addEventListener('click', () => {
-    
-    
-  });
+  const button = document.querySelector('.button');
+  
+  
 
+  // button.addEventListener('click', () => {
+  //   console.log('click 2');
+  // });
+
+  
   form.addEventListener('submit', (e) => {
     e.preventDefault();
+  
+    if (form.checkValidity()) {
+      console.log('valid');
+    } else {
+      console.log('invalid');    
+    }
 
+    const elements = form.elements;
+    
+    elements.forEach((el) => {
+      console.dir(el);
+    });
+  
     console.log('submit');
   });
 });
