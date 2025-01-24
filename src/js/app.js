cd4ef98a99541d495a84e745ca376bd552499335
Maksim Chalyh <nazgulius@yandex.ru>
2025-01-25 00:04:26 +0300

    update app.js and Widget.js

--- src/js/app.js
+++ src/js/app.js
@@ -16,39 +16,52 @@ const errors = {
   },
 }
 
-document.addEventListener('DOMContentLoaded', () => { 
-  
-
-  const button = document.querySelector('.button');
-  
-  
-
-  // button.addEventListener('click', () => {
-  //   console.log('click 2');
-  // });
 
   
-  form.addEventListener('submit', (e) => {
-    e.preventDefault();
-  
-    if (form.checkValidity()) {
-      console.log('valid');
-    } else {
-      console.log('invalid');    
-    }
 
-    const elements = Array.from(form.elements);
-    elements.forEach((el) => {
+const button = document.querySelector('.button');
+
+const widgetFactory = new Widget();
+
+const actualMessage = [];
+
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  actualMessage.forEach((id) => widgetFactory.removeWidget(id));
+
+  if (form.checkValidity()) {
+    console.log('valid');
+  } else {
+    console.log('invalid');    
+  }
+
+  const elements = Array.from(form.elements);
+  
+  [...elements].some((el) => {
+    return Object.keys(ValidityState.prototype).some((key) => {
       if (!el.name) return;
-      for (const key in el.validity) {
+      if (key === 'valid') return;
+
+      //for (const key in el.validity) {
         if (el.validity[key]){
-          console.dir(el);
+          console.dir(key);
           console.dir(errors[el.name][key]);
+
+          actualMessage.push(widgetFactory.showWidget(errors[el.name][key], el));
+
+          return true;
         }
-      }
-     
+      //}
+
     });
-  
-    console.log('submit');
   });
+
+
+  console.log('submit');
+});
+
+document.addEventListener('DOMContentLoaded', () => { 
+
 });
